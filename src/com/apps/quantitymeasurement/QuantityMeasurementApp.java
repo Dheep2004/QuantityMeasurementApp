d1d8aa038c1b60b4f4824ad04c536930b24506b1
Dheep2004 <dheepthakansal02@gmail.com>
2026-05-04 23:04:29 +0530

    UC2: Added Inches equality support

--- src/com/apps/quantitymeasurement/QuantityMeasurementApp.java
+++ src/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -24,30 +24,30 @@ public class QuantityMeasurementApp {
                 return Double.compare(this.value, other.value) == 0;
             }
         }
-//        public static class Inches{
-//            private final double value;
-//            public Inches(double value) {
-//                this.value = value;
-//            }
-//            @Override
-//            public boolean equals(Object obj){
-//                if(this == obj) return true;
-//                if(obj == null) return false;
-//                if(this.getClass() != obj.getClass()) return false;
-//                Inches other = (Inches) obj;
-//                return Double.compare(this.value, other.value) == 0;
-//            }
-//        }
+        public static class Inches{
+            private final double value;
+            public Inches(double value) {
+                this.value = value;
+            }
+            @Override
+            public boolean equals(Object obj){
+                if(this == obj) return true;
+                if(obj == null) return false;
+                if(this.getClass() != obj.getClass()) return false;
+                Inches other = (Inches) obj;
+                return Double.compare(this.value, other.value) == 0;
+            }
+        }
 
         public static void main(String[] args) {
             Feet f1 = new Feet(1.0);
             Feet f2 = new Feet(1.0);
 
-//            Inches I1 = new Inches(1.0);
-//            Inches I2 = new Inches(1.0);
+            Inches I1 = new Inches(1.0);
+            Inches I2 = new Inches(1.0);
 
             System.out.println(f1.equals(f2));
-//            System.out.println(I1.equals(I2));
+            System.out.println(I1.equals(I2));
         }
     }
 
--- src/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
+++ src/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 import com.apps.quantitymeasurement.QuantityMeasurementApp.Feet;
+import com.apps.quantitymeasurement.QuantityMeasurementApp.Inches;
 
 public class QuantityMeasurementAppTest {
 
@@ -61,5 +62,37 @@ public class QuantityMeasurementAppTest {
     void testFeetEquality_SameReference() {
         Feet f1 = new Feet(1.0);  //same object should always equal to itself (if (this == obj) return true)
         assertTrue(f1.equals(f1));
+    }
+    @Test
+    void testInchesEquality_SameValue() {
+        Inches i1 = new Inches(1.0);
+        Inches i2 = new Inches(1.0); //ensure true same values ke liye
+        assertTrue(i1.equals(i2));
+    }
+
+    @Test
+    void testInchesEquality_DifferentValue() {  // different values ke liye false
+        Inches i1 = new Inches(1.0);
+        Inches i2 = new Inches(2.0);
+        assertFalse(i1.equals(i2));
+    }
+
+    @Test
+    void testInchesEquality_NullComparison() {  //koi value nill na ho comparision mai
+        Inches i1 = new Inches(1.0);
+        assertFalse(i1.equals(null));
+    }
+
+    @Test
+    void testInchesEquality_DifferentClass() {
+        Inches i1 = new Inches(1.0);    //return type hi alag hai
+        String str = "1.0";
+        assertFalse(i1.equals(str));
+    }
+
+    @Test
+    void testInchesEquality_SameReference() {
+        Inches i1 = new Inches(1.0);
+        assertTrue(i1.equals(i1));      //agr same object reference ho to true aaye confuse na ho
     }
 }
